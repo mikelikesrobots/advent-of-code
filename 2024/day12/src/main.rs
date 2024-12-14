2479a35e88c12a85a8a7a862ac6ac25f1ce39148
Mike Likes Robots <mikelikesrobots@outlook.com>
2024-12-14 11:09:20 +0000

    Complete Day 12

--- 2024/day12/src/main.rs
+++ 2024/day12/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::{collections::HashMap, str::FromStr};
 
@@ -85,20 +86,119 @@ impl PlantMap {
         }
         perim
     }
+    fn perimeter_with_discount(region: &[Point]) -> usize {
+        let mut fences = 0;
+
+        // Scan from top to bottom
+        let mut current_row: Vec<i32> = vec![];
+        let mut prev_row: Vec<i32> = vec![];
+        let mut points = region.to_vec();
+        points.sort();
+
+        let count_distinct = |v: &Vec<i32>| {
+            let mut distinct_vals = 0;
+            let mut prev_val = None;
+            for val in v {
+                if let Some(prev) = prev_val {
+                    if val.abs_diff(prev) != 1 {
+                        distinct_vals += 1;
+                    }
+                    prev_val = Some(*val);
+                } else {
+                    distinct_vals += 1;
+                    prev_val = Some(*val);
+                }
+            }
+            distinct_vals
+        };
+
+        let extra_fences = |current_row: &Vec<i32>, prev_row: &Vec<i32>| {
+            // Diff the rows
+            let new_current_vals: Vec<i32> = current_row.iter().filter(|p| !prev_row.contains(p)).copied().collect();
+            let new_prev_vals: Vec<i32> = prev_row.iter().filter(|p| !current_row.contains(p)).copied().collect();
+
+            // Return distinct count from both vector
+            count_distinct(&new_current_vals) + count_distinct(&new_prev_vals)
+        };
+
+        let mut prev_row_idx = -1;
+        while let Some(point) = points.pop() {
+            let current_row_idx = point.0;
+            if current_row_idx != prev_row_idx {
+                fences += extra_fences(&current_row, &prev_row);
+
+                prev_row = current_row.to_vec();
+                current_row.clear();
+                current_row.push(point.1);
+                prev_row_idx = current_row_idx;
+            } else {
+                current_row.push(point.1);
+            }
+        }
+
+        fences += extra_fences(&prev_row, &current_row);
+        fences += extra_fences(&current_row, &vec![]);
+
+        // Scan from left to right
+        let mut points = region.to_vec();
+        points.sort_by(|a, b| {
+            match (a.0.cmp(&b.0), a.1.cmp(&b.1)) {
+                (_, Ordering::Greater) => Ordering::Greater,
+                (_, Ordering::Less) => Ordering::Less,
+                (Ordering::Greater, _) => Ordering::Greater,
+                (Ordering::Less, _) => Ordering::Less,
+                (Ordering::Equal, Ordering::Equal) => Ordering::Equal,
+            }
+        });
+
+        let mut prev_col_idx = -1;
+        let mut prev_col = vec![];
+        let mut current_col = vec![];
+        while let Some(point) = points.pop() {
+            let current_col_idx = point.1;
+            if current_col_idx != prev_col_idx {
+                fences += extra_fences(&current_col, &prev_col);
+
+                prev_col = current_col.to_vec();
+                current_col.clear();
+                current_col.push(point.0);
+                prev_col_idx = current_col_idx;
+            } else {
+                current_col.push(point.0);
+            }
+        }
+
+        fences += extra_fences(&prev_col, &current_col);
+        fences += extra_fences(&current_col, &vec![]);
+
+        fences
+    }
     fn cost(region: &[Point]) -> usize {
         PlantMap::area(region) * PlantMap::perimeter(region)
     }
+    fn cost_with_discount(region: &[Point]) -> usize {
+        PlantMap::area(region) * PlantMap::perimeter_with_discount(region)
+    }
 
-    fn part_a(&self) -> usize {
+    fn calc_cost<F>(&self, cost_fn: F) -> usize
+    where F: Fn(&Vec<Point>) -> usize {
         self.regions
             .values()
             .map(|regions| {
                 regions
                     .iter()
-                    .map(|region| PlantMap::cost(region))
+                    .map(&cost_fn)
                     .sum::<usize>()
             })
             .sum()
+    }
+
+    fn part_a(&self) -> usize {
+        self.calc_cost(|r| PlantMap::cost(r))
+    }
+
+    fn part_b(&self) -> usize {
+        self.calc_cost(|r| PlantMap::cost_with_discount(r))
     }
 }
 
@@ -106,6 +206,7 @@ fn main() {
     let puzzle = include_str!("../puzzle/input.txt");
     let map = PlantMap::from_str(puzzle).unwrap();
     println!("Part A: {}", map.part_a());
+    println!("Part B: {}", map.part_b());
 }
 
 #[cfg(test)]
@@ -113,7 +214,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_test_1_txt() {
+    fn test_test_1_txt_part_a() {
         let puzzle = include_str!("../puzzle/test_1.txt");
         let map = PlantMap::from_str(puzzle).unwrap();
         let result = map.part_a();
@@ -121,7 +222,7 @@ mod tests {
     }
 
     #[test]
-    fn test_test_2_txt() {
+    fn test_test_2_txt_part_a() {
         let puzzle = include_str!("../puzzle/test_2.txt");
         let map = PlantMap::from_str(puzzle).unwrap();
         let result = map.part_a();
@@ -129,10 +230,34 @@ mod tests {
     }
 
     #[test]
-    fn test_test_3_txt() {
+    fn test_test_3_txt_part_a() {
         let puzzle = include_str!("../puzzle/test_3.txt");
         let map = PlantMap::from_str(puzzle).unwrap();
         let result = map.part_a();
         assert_eq!(1930, result);
+    }
+
+    #[test]
+    fn test_test_1_txt_part_b() {
+        let puzzle = include_str!("../puzzle/test_1.txt");
+        let map = PlantMap::from_str(puzzle).unwrap();
+        let result = map.part_b();
+        assert_eq!(80, result);
+    }
+
+    #[test]
+    fn test_test_2_txt_part_b() {
+        let puzzle = include_str!("../puzzle/test_2.txt");
+        let map = PlantMap::from_str(puzzle).unwrap();
+        let result = map.part_b();
+        assert_eq!(436, result);
+    }
+
+    #[test]
+    fn test_test_3_txt_part_b() {
+        let puzzle = include_str!("../puzzle/test_3.txt");
+        let map = PlantMap::from_str(puzzle).unwrap();
+        let result = map.part_b();
+        assert_eq!(1206, result);
     }
 }
--- 2024/util/src/point.rs
+++ 2024/util/src/point.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, PartialOrd, Ord)]
 pub struct Point(pub i32, pub i32);
 impl Point {
     pub fn add(&self, point: &Point) -> Point {
@@ -13,3 +13,15 @@ impl Point {
         Point(point.0 - self.0, point.1 - self.1)
     }
 }
+
+// impl Ord for Point {
+//     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+//         match (self.0.cmp(&other.0), self.1.cmp(&other.1)) {
+//             (Ordering::Greater, _) => Ordering::Greater,
+//             (Ordering::Less, _) => Ordering::Less,
+//             (_, Ordering::Greater) => Ordering::Greater,
+//             (_, Ordering::Less) => Ordering::Less,
+//             (Ordering::Equal, Ordering::Equal) => Ordering::Equal,
+//         }
+//     }
+// }
